b4bc0d62458b7a5c510b8542712bfead17bf2666
Sujana798 <bsse1618@iit.du.ac.bd>
2026-01-07 21:16:02 +0600

    Update spl.c

--- spl.c
+++ spl.c
@@ -49,3 +49,55 @@ char* clean_text_data(char* text, long size, long* clean_len) {
     
     return clean;
 }
+
+char** extract_words(char* clean, long clean_len, int* count) {
+    char** words = NULL;
+    int capacity = 1000;
+    *count = 0;
+    
+    words = malloc(capacity * sizeof(char*));
+    
+    long i = 0;
+    while(i < clean_len) {
+        
+        while(i < clean_len && clean[i] == ' ') {
+            i++;
+        }
+        
+        if(i >= clean_len) {
+            break;
+        }
+        
+        long start = i;
+        while(i < clean_len && clean[i] != ' ') {
+            i++;
+        }
+        
+        long len = i - start;
+        
+        if(len > 0 && len < 100) {
+           
+            if(*count >= capacity) {
+                capacity *= 2;
+                words = realloc(words, capacity * sizeof(char*));
+            }
+            
+            words[*count] = malloc(len + 1);
+            strncpy(words[*count], clean + start, len);
+            words[*count][len] = '\0';
+            (*count)++;
+        }
+    }
+    
+    return words;
+}
+
+void initialize_buckets(int book_id, int word_count) {
+    all_buckets[book_id] = malloc(26 * sizeof(char**));
+    memset(bucket_sizes[book_id], 0, sizeof(bucket_sizes[book_id]));
+    
+    for(int b = 0; b < 26; b++) {
+        all_buckets[book_id][b] = malloc(word_count * sizeof(char*));
+    }
+}
+
